2c298f259e69646e287b6f92ad07299f3c47b15a
Miguel <fusionblur17@gmail.com>
2022-05-22 21:48:24 +0200

    Agregando Comparadores

--- src/Funciones/Comparador.java
+++ src/Funciones/Comparador.java
@@ -24,4 +24,3 @@ public class Comparador implements Comparator<String> {
     }
     
 }
-
--- src/Funciones/OrdenadorNatural.java
+++ src/Funciones/OrdenadorNatural.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Funciones;
 
 /**
- *
+ * Aqui tenemos una clase de ejemplo en la que implementamos el comparable, 
+ * como vemos tiene un atributo por el cual vamos a establecer el orden natural
+ * de nuestra clase. 
+ * 
+ * Solamente tenemos que implementar el método compareTo que 
+ * en este caso al ser de un int con devolver una resta, si es negativo será menor
+ * si es 0 será igual y si es positivo será mayor.
+ * 
+ * En caso de ser otra clase deberemos estipular nosotros y hacer una cadena de ifs,
+ * en el caso de ser String es tan fácil como devolver una compareTo de los dos
+ * Strings.
  * @author Migue
  */
-public class OrdenadorNatural {
+
+public class OrdenadorNatural implements Comparable {
+
+    private int atributoEjemplo;
     
+    public OrdenadorNatural(){
+        atributoEjemplo = 0;
+    }
+    
+    @Override
+    public int compareTo(Object o) {
+        return this.getAtributoEjemplo()-((OrdenadorNatural)o).getAtributoEjemplo();
+    }
+    
+    public int getAtributoEjemplo(){
+        return this.atributoEjemplo;
+    }
 }
